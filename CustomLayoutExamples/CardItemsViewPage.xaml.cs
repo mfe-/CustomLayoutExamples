3e41de6e74d04491ec117929858a3c9577f618ee
Martin Fetzel <mfe@live.de>
2022-07-17 12:03:13 +0200

    init

--- CustomLayoutExamples/CardItemsViewPage.xaml.cs
+++ CustomLayoutExamples/CardItemsViewPage.xaml.cs
@@ -7,5 +7,14 @@ public partial class CardItemsViewPage : ContentPage
 	public CardItemsViewPage()
 	{
 		InitializeComponent();
+		Loaded += CardItemsViewPage_Loaded;
+	}
+
+	private void CardItemsViewPage_Loaded(object? sender, EventArgs e)
+	{
+		if(this.BindingContext is CardItemsViewPageViewModel cardItemsViewPageViewModel)
+		{
+			cardItemsViewPageViewModel.Page = this;
+		}
 	}
 }
--- CustomLayoutExamples/CardItemsViewPageViewModel.cs
+++ CustomLayoutExamples/CardItemsViewPageViewModel.cs
@@ -3,36 +3,76 @@ using Microsoft.Maui.Controls;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace CustomLayoutExamples;
 
-public class CardItemsViewPageViewModel
+public class CardItemsViewPageViewModel : INotifyPropertyChanged
 {
+    SynchronizationContext synchronizationContext = SynchronizationContext.Current;
     public CardItemsViewPageViewModel()
     {
         RemoveCommand = new Command<object>(RemoveItem);
         AddCommand = new Command<object>(AddItem);
+
+
     }
-    public ObservableCollection<Card> Cards { get; set; } = new ObservableCollection<Card>()
+    private void InitCards()
     {
-        new Card(1),
-        new Card(2),
-        new Card(3),
-        new Card(4),
-        new Card(4),
-        new Card(4),
-        new Card(4),
-        new Card(4),
-        new Card(4),
-        new Card(4),
-        new Card(4),
-        new Card(4),
-        new Card(4)
-    };
+        Task.Run(async () =>
+        {
+            try
+            {
+                await Task.Delay(TimeSpan.FromSeconds(5));
+                Page.Dispatcher.Dispatch(Init);
+                //synchronizationContext.Post((o) => Init(), null);
+                //Init();
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e);
+            }
+
+        });
+        //Init();
+    }
+
+    private Page _Page;
+    public Page Page
+    {
+        get { return _Page; }
+        set
+        {
+            SetProperty(ref _Page, value, nameof(Page));
+            InitCards();
+        }
+    }
+
+    private void Init()
+    {
+        Cards = new ObservableCollection<Card>()
+        {
+            new Card(1),
+            new Card(2),
+            new Card(3),
+            new Card(4),
+            new Card(4),
+            new Card(4),
+            new Card(4),
+            new Card(4),
+            new Card(4),
+            new Card(4),
+            new Card(4),
+            new Card(4),
+            new Card(4)
+        };
+    }
+    public ObservableCollection<Card> Cards { get; set; }
 
     public ICommand RemoveCommand { get; }
     private void RemoveItem(object obj)
@@ -47,5 +87,19 @@ public class CardItemsViewPageViewModel
     {
         Card card = new Card(9);
         Cards.Add(card);
+    }
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        var handler = PropertyChanged;
+        if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+    }
+    protected bool SetProperty<T>(ref T field, T value, string propertyName)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
     }
 }
